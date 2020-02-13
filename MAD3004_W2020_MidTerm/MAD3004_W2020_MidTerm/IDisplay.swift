e620f3a9805e557498833c5db74873b0ec4cadb8
GagandeepKaur97 <sherry2312.gk@gmail.com>
2020-02-13 18:21:18 -0500

    [gagandeepkaur] protocol  created

---
+++ MAD3004_W2020_MidTerm/MAD3004_W2020_MidTerm/IDisplay.swift
@@ -0,0 +1,13 @@
+//
+//  IDisplay.swift
+//  MAD3004_W2020_MidTerm
+//
+//  Created by gagandeep kaur on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+protocol IDisplay {
+    func Display()
+}
