a916e23efb134396f1fb0ac09eb1d2597e5bf6a3
GagandeepKaur97 <sherry2312.gk@gmail.com>
2020-02-13 18:14:07 -0500

    [gagandeep kaur] Bill class created

---
+++ MAD3004_W2020_MidTerm/MAD3004_W2020_MidTerm/Bill.swift
@@ -0,0 +1,12 @@
+//
+//  Bill.swift
+//  MAD3004_W2020_MidTerm
+//
+//  Created by gagandeep kaur on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Bill
+
