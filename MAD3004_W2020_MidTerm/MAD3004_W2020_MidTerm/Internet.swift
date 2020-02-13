5e25b6135fe198af52de383bde1fefa9c51fb0d4
GagandeepKaur97 <sherry2312.gk@gmail.com>
2020-02-13 18:36:18 -0500

    [gagandeepkaur] class internet created with required attributes.

---
+++ MAD3004_W2020_MidTerm/MAD3004_W2020_MidTerm/Internet.swift
@@ -0,0 +1,30 @@
+//
+//  Internet.swift
+//  MAD3004_W2020_MidTerm
+//
+//  Created by gagandeep kaur on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Internet : Bill
+{
+    var providerName  : String
+    var internetGb    : String
+    
+        init(billId: Int, billDate: Date, billType: String, totalBillAmount: Double , providerName : String , internetGb: String)
+    {
+        self.providerName = providerName
+        self.internetGb = internetGb
+        super.init (billId: billId , billDate: billDate, billType: billType, totalBillAmount: totalBillAmount)
+    }
+    
+   override func Display()
+   {
+    super.Display()
+    
+    print(" providerName        : \(self.providerName)")
+    print(" internetGb          : \(self.internetGb)")
+    }
+    }
