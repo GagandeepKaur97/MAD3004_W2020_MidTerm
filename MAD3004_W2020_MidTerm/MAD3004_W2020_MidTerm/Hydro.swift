d8710c9fad0be88daa898e11c4d35c07f2214f25
GagandeepKaur97 <sherry2312.gk@gmail.com>
2020-02-13 19:03:05 -0500

    [gagandeepkaur]class created

---
+++ MAD3004_W2020_MidTerm/MAD3004_W2020_MidTerm/Hydro.swift
@@ -0,0 +1,12 @@
+//
+//  Hydro.swift
+//  MAD3004_W2020_MidTerm
+//
+//  Created by gagandeepkaur on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Hydro : Bill
+
