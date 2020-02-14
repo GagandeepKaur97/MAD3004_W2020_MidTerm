2002e5f7415fa80f90b753d34053a4360fa569e3
GagandeepKaur97 <sherry2312.gk@gmail.com>
2020-02-13 19:23:43 -0500

    [gagandeep kaur] class customer created with initializer

---
+++ MAD3004_W2020_MidTerm/MAD3004_W2020_MidTerm/Customer.swift
@@ -0,0 +1,35 @@
+//
+//  Customer.swift
+//  MAD3004_W2020_MidTerm
+//
+//  Created by Gagandeep kaur on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Customer : Bill
+{
+    var customerId : Int
+    var firstName  : String
+    var lastName   : String
+    var fullName   : String
+    var emailId    : String
+    var totalAmountToPay: Double
+    
+    init(billId: Int, billDate: Date, billType: String, totalBillAmount: Double ,customerId : Int ,firstName  : String,lastName   : String , fullName   : String ,emailId    : String ,totalAmountToPay: Double)
+        
+      {
+        self.customerId       = customerId
+        self.firstName        = firstName
+        self.lastName         = lastName
+        self.fullName         = fullName
+        self.emailId           = emailId
+        self.totalAmountToPay  = totalAmountToPay
+        super.init(billId: billId, billDate: billDate, billType: billType, totalBillAmount: totalBillAmount )
+        
+    }
+    override func Display() {
+        super.Display()
+    }
+}
