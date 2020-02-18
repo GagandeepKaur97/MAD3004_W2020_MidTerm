c3266426d64ec447878d0f3dd081de1a38b2ef75
GagandeepKaur97 <sherry2312.gk@gmail.com>
2020-02-18 16:15:40 -0500

    [gagandeep kaur] extensionint added

---
+++ MAD3004_W2020_MidTerm/MAD3004_W2020_MidTerm/Extension.Int.swift
@@ -0,0 +1,24 @@
+//
+//  Extension.Int.swift
+//  MAD3004_W2020_MidTerm
+//
+//  Created by MacStudent on 2020-02-18.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+extension Int {
+       func h()-> String {
+           return "\(self)Units"
+       }
+       func m ()->String
+       {
+           return"\(self)minutes"
+        
+    }
+       func i () ->String{
+           return "\(self)GB"}
+       func b()->String{
+           return "$\(self)"}
+       }
+   
